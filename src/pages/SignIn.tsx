9805e0411c078c6923751c114b90c0618431dd37
Kim SeungMoo <tmdan1346@gmail.com>
2021-06-28 14:34:44 +0900

    [client] feat/navbar

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function SignIn() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="navbar__body">
+      <button
+      className="navbar__signin-btn"
+      onClick={() => setIsVisible(true)}
+      >
+        SignIn
+      </button>
+      <div className={isVisible? "show": "hide"}>
+        <div className="modal__overlay" onClick={() => setIsVisible(false)}></div>
+        <div className="modal__content">
+          <input
+          placeholder="EMAIL"
+          ></input>
+          <input
+          placeholder="PASSWORD"
+          ></input>
+          <button>SignIn</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn;

792fa70f6009609bdb1d132c6538c2dd34ebacfd
dong-geon-Lee <ch9716@naver.com>
2021-06-29 16:48:58 +0900

    [client] pages/Header

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -5,7 +5,6 @@ const Header: React.FC = () => {
   return (
     <section id="header">
       <div className="header__container">
-        <img src="images/4forhobby.png" alt="" className="header__logo" />
         <div className="header__text">
           <h1>
             기억해주세요! <br></br>사건 & 사고들을!
@@ -19,23 +18,6 @@ const Header: React.FC = () => {
           <button className="header__btn__top">자세히 보기</button>
         </div>
       </div>
-
-      <nav>
-        <ul>
-          <li>
-            <a href="#">소개</a>
-          </li>
-          <li>
-            <a href="#">활동</a>
-          </li>
-          <li>
-            <a href="#">참여</a>
-          </li>
-          <li>
-            <a href="#">후원안내</a>
-          </li>
-        </ul>
-      </nav>
     </section>
   );
 };

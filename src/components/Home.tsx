c2e76ba4ee5b6845da200b673bdea441e5815fce
dong-geon-Lee <ch9716@naver.com>
2021-06-29 17:33:51 +0900

    [client] pages/Party

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import Header from "../pages/Header";
 import About from "../pages/About";
 import TodoIntro from "../pages/TodoIntro";
+import Party from "../pages/Party";
 
 const Home: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const Home: React.FC = () => {
       <Header></Header>
       <About></About>
       <TodoIntro></TodoIntro>
+      <Party></Party>
     </>
   );
 };

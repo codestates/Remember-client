408d47fd23aed6930d2ac484bc3272015104b06d
Lee Joo Hyung <wngud985@gmail.com>
2021-07-12 14:38:10 +0900

    [client] feat/about add about1,2,8

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Home.css";
 import Header from "../pages/Header";
-import About from "../pages/About";
+import About1 from "../pages/About1";
+import About2 from "../pages/About2";
+import About8 from "../pages/About8";
 import TodoIntro from "../pages/TodoIntro";
 import Party from "../pages/Party";
 import JoinPage from "../pages/JoinPage";
@@ -11,7 +13,9 @@ const Home: React.FC = () => {
   return (
     <>
       <Header></Header>
-      <About></About>
+      <About1></About1>
+      <About2></About2>
+      <About8></About8>
       <TodoIntro></TodoIntro>
       <Party></Party>
       <JoinPage></JoinPage>
